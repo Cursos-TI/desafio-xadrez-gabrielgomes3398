d6d77b9881291ca4ed73ba1dede20a8091ec2f95
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-02-26 23:20:50 +0000

    //Mover o bispo 5 casas na diagonal cime e á direira

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,10 @@ int main() {
 for (int i = 0; i < 5; i++){ 
     printf("Direira\n"); //imprime a direcao do movimento 
 }
+   //Mover o bispo 5 casas na diagonal cime e á direira
+for (int i = 0; i< 5; i++){
+    printf("Cima, Direita\n"); //imprime a direcao do movimento
+}   
 
     return 0;
 }
