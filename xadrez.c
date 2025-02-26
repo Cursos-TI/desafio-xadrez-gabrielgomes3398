e13e3b8ee5b0defffb6890fa62c3d347e51bf456
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-02-26 23:10:51 +0000

    for (int i = 0; i < 5; i++){

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   //Mover a Torre 5 casas para a direira
+   //Mover a Torre 5 casas para a direira 
 for (int i = 0; i < 5; i++){
     printf("Direira\n"); //imprime a direcao do movimento
 }
