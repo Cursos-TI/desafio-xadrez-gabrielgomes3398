277ff629140c46f9b75fd9d013eac2b33d5f183e
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-03-03 21:53:50 +0000

    for (int i = 0; i < 2; i++) {

--- xadrez.c
+++ xadrez.c
@@ -5,7 +5,7 @@ int main() {
         
         while (movimentoCompleto --) 
          {
-             for (int i = 0; i < 2; i++) {
+             for (int i = 0; i < 2; i++) { 
                   printf("Baixo\n"); // Imprime "Baixo" duas vezes 
              }
              printf("Esquerda\n"); // Imprime "Esquerda" uma vez 
