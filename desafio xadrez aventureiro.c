522ae20a82727297b7e36e8327d0b9ca399aa9e5
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-03-03 21:46:35 +0000

    }

--- desafio xadrez aventureiro.c
+++ desafio xadrez aventureiro.c
@@ -6,7 +6,7 @@ int main(){
     while (movimentoCompleto --)
      {
          for (int i = 0; i < 2; i++) {
-              printf("Baixo\n"); // Imprime "Baixo" duas vezes
+              printf("Baixo\n"); // Imprime "Baixo" duas vezes 
          }
          printf("Esquerda\n"); // Imprime "Esquerda" uma vez
     }
