e900af9a189cdca3ac30f89bfd4311dc75729c71
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-03-03 21:47:47 +0000

    return 0;

--- desafio xadrez aventureiro.c
+++ desafio xadrez aventureiro.c
@@ -9,6 +9,6 @@ int main(){
               printf("Baixo\n"); // Imprime "Baixo" duas vezes 
          }
          printf("Esquerda\n"); // Imprime "Esquerda" uma vez 
-    }
+    } 
   return 0;
 } 
