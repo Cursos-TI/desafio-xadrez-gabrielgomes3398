b613666b8fadf85241879f2c5432420b6041a991
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-03-03 20:54:47 +0000

    for(int i = 0; i < 2; i++) {

--- xadrez.c
+++ xadrez.c
@@ -26,9 +26,9 @@ for (int i = 0; i < 8; i++){
 
    while (movimentocompleto --)
    {
-       for(int i = 0; i < 2; i++) {
+       for(int i = 0; i < 2; i++) { 
            printf("baixo\n"); // imprime "baixo" duas vezes
        }
-          
+
     return 0;
 }
